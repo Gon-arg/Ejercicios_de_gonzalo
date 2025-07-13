b9de7eda6ada2c1a4249a072a25f1fa1c5dff571
Gonzalo Herrera <mrgonzalo223@gmail.com>
2025-07-13 00:16:41 -0300

    rama1

--- practica_punto_1_Arreglos/obj/Debug/net9.0/practica_punto_1_Arreglos.AssemblyInfo.cs
+++ practica_punto_1_Arreglos/obj/Debug/net9.0/practica_punto_1_Arreglos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practica_punto_1_Arreglos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed3957566da765aff8671e07ea7bce306f676a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("practica_punto_1_Arreglos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practica_punto_1_Arreglos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
